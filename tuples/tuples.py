475b2fd6975d00120861d39343f5b1de4d110ddb
K-Honsu <feranmia51@gmail.com>
2023-08-03 23:40:18 +0100

    add tuples

--- tuples/tuples.py
+++ tuples/tuples.py
@@ -8,4 +8,30 @@ print(newTuple)
 # we can also use the tuple keyword
 newTuple1 = tuple('abcde')
 print(newTuple1)
-print(newTuple)
+
+# accessing elements with tuples using []
+print(tupel[1])
+# we can also use slice operator to slice elements
+
+
+# Traversing elements in tuple
+for i in tupel:
+    # print(i)
+    pass
+for i in range(len(tupel)):
+    # print(tupel[i])
+    pass
+
+# Searching for elements in tuple
+# method 1 -> using "in" operator
+print('b' in tupel)
+# method 2 -> using "index" method -> this is to get the index number
+print(tupel.index('c'))
+# method 3 -> using function
+def searchTuple(tup, element):
+    for i in range(len(tup)):
+        if tup[i] == element:
+            return f"The element {element} is located at index {i}"
+    return f"The element {element} was not found"
+
+print(searchTuple(tupel, 'c'))
